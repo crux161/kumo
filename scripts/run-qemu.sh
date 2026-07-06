366b3c96a2247c213416d2863545a7f67debbf51
crux161 <eain161@gmail.com>
2026-07-05 23:50:22 -0700

    j417 the horror...touchpad drivers diverted

--- scripts/run-qemu.sh
+++ scripts/run-qemu.sh
@@ -4,7 +4,8 @@
 
 qemu-system-aarch64 -M virt,gic-version=3 \
 	-cpu cortex-a72 -m 512 -display none \
-	-serial stdio -monitor none   \
+	-serial stdio \
+        -monitor none   \
 	-drive if=pflash,format=raw,file=/opt/homebrew/share/qemu/edk2-aarch64-code.fd,readonly=on \
 	-drive if=pflash,format=raw,file=./kumo-vars.fd  \
 	-drive file=fat:rw:/Users/crux/git/soso/SANITARY/kumo/build/images/qemu-virt-aarch64,format=raw,if=virtio
